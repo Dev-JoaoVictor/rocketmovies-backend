fcadbb0f10bd1f374458a5cdbb675f74f7b9c2de
João <joaovictor.ramos34@gmail.com>
2023-06-13 10:47:42 -0300

    Utilizando o metodo GET

--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 
-const PORT = 3333;
+app.get("/", (request, response) => {
+  response.send("Hello World");
+});
 
+const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
