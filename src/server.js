ca6ebc4bb9dd15b3cdae551847bd7706d2a0fd35
João <joaovictor.ramos34@gmail.com>
2023-06-13 11:00:38 -0300

    Utilizando parâmetros nas rotas

--- src/server.js
+++ src/server.js
@@ -2,8 +2,12 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
-  response.send("Hello World");
+app.get("/teste/:id/:user", (request, response) => {
+  const { id, user } = request.params;
+  response.send(`
+    Id da mensagem ${id}
+    Para o Usuário ${user}
+  `);
 });
 
 const PORT = 3333;
